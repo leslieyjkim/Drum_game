d9edaebc935f3488ed4f29a3bd5f1f1282e37985
yoonjookim <yjblossom@gmail.com>
2024-08-05 20:13:25 -0400

    Test added eventlistener with function of handleClick

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+document.querySelector("button").addEventListener("click", handleClick);
 
+function handleClick() {
+    alert("I got clicked");
+}
 
 
 // alert("test!"); //for test linked up properly in index.html
