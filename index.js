ff7bfb8049f304c997a9806d5aacdc487683e58b
yoonjookim <yjblossom@gmail.com>
2024-08-05 22:31:17 -0400

    Added first drum's audio sound play() & background image each button

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const drumButtons = document.querySelectorAll(".drum");
+
 const numberOfDrumButtons = drumButtons.length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
     drumButtons[i].addEventListener("click", function() {
-        alert("I got clicked");
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
     });
 }
 
